8748f6ff656806e7351ca31f0f48f68311676ae5
Reet Chahil <chahilr@outlook.com>
2023-07-20 22:40:26 -0400

    Created signup and logout hooks

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
+import { useSignup } from '../hooks/useSignup';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await signup(email, password);
   };
 
   return (
@@ -23,10 +25,10 @@ const Signup = () => {
       <input
         type="password"
         onChange={(e) => setPassword(e.target.value)}
-        value={email}
+        value={password}
       />
-
-      <button>Sign up</button>
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
