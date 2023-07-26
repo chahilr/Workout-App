28de03695acbe095658046a8e51a8480d6fa8435
Reet Chahil <chahilr@outlook.com>
2023-07-26 17:03:31 -0400

    protected react routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // pages & components
 import Home from './pages/Home';
@@ -15,9 +15,18 @@ function App() {
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={user ? <Home /> : <Login />} />
-            <Route path="/login" element={user ? <Home /> : <Login />} />
-            <Route path="/signup" element={user ? <Home /> : <Signup />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
